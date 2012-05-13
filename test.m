36f90285ec223137c96958e17828e731c8becd02
Jaka Hudoklin <jakahudoklin@gmail.com>
2012-05-13 10:04:44 -0500

    wtf test patch

--- test.m
+++ test.m
@@ -8,7 +8,8 @@ frequency= sample_frequency/2;
 n=filtfilt(b,a,m);
 [frames,resample_time, colors] = video_extract(n);
 [Y,C]=yc_demodulator(frames,resample_time);
-%[YUV]=chroma(Y,C,colors,resample_time);
+[YUV]=chroma(Y,C,colors,resample_time);
+imshow(YUV)
 %newf = C(:,:,1) .+ abs(min(min(C(:,:,1))));
 %new2 = newf ./ max(max(newf));
 %[m,n]=size(new2)
