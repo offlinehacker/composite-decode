7267989de03e71f7b839d116d274c41f8a8653c4
Jaka Hudoklin <jakahudoklin@gmail.com>
2012-05-13 03:59:12 -0500

    Test patch

--- test.m
+++ test.m
@@ -1,7 +1,7 @@
 m = load("tests/C1PAL00005.dat");
 [frames, colors] = video_extract(m);
-newf = frames .+ abs(min(min(frames)));
-new2 = newf ./ max(max(newf))
+newf = frames(:,:,1) .+ abs(min(min(frames(:,:,1))));
+new2 = newf ./ max(max(newf));
 imshow(new2)
 figure;
 plot(m)
